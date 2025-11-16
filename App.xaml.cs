ea753a369cbe8ef972ecc6927e4f0494792551c3
KamilSzat <uT_3776@zsp3zamosc.pl>
2025-11-16 21:05:07 +0100

    Auta maui wreszcie skonczone

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace auta
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace auta
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
